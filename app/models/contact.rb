130d690fe7bd429a46201d48489080af15bc3568
廣本 和也 <hiromoto.kazuya@gmail.com>
2018-10-16 15:04:04 +0900

    create mailer-function(developent only)

---
+++ app/models/contact.rb
@@ -0,0 +1,6 @@
+class Contact < ApplicationRecord
+  validates :name, presence:true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  validates :content, presence:true
+end
